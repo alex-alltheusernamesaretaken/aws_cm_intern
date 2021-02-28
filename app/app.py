41d3bbef4f63fbec589b0e80c5d42d8764ba4554
alex <admin36@gmail.com>
2021-02-28 14:44:52 -0500

    now its a git

--- app/app.py
+++ app/app.py
@@ -1,15 +1,21 @@
 import requests
 import boto3
 
-DRY_RUN = False  # Sets the dry run flag on the aws calls
-GROUP_NAME = 'atlassian_id'  # the name of the security group we're updating
-VERBOSE = False
+DRY_RUN_DEFAULT = False  # Sets the default dry run flag on the aws calls
+GROUP_NAME_DEFAULT = 'atlassian_id'  # the default name of the security group we're updating
+VERBOSE_DEFAULT = False  # default setting for verbosity
+IPRANGE_URL_DEFAULT = "https://ip-ranges.atlassian.com/"
 
 
 def lambda_handler(event, context):
+    # set up some values using the arguments from event, otherwise use default values defined above
+    dry_run = True if "DRY_RUN" in event and event["DRY_RUN"].lower() == "true" else DRY_RUN_DEFAULT
+    verbose = True if "VERBOSE" in event and event["VERBOSE"].lower() == "true" else VERBOSE_DEFAULT
+    group_name = GROUP_NAME_DEFAULT if "GROUP_NAME" not in event else event["GROUP_NAME"]
+    iprange_url = IPRANGE_URL_DEFAULT if "IPRANGE_URL" not in event else event["IPRANGE_URL"]
 
     try:
-        r = requests.get("https://ip-ranges.atlassian.com/")  # make the request to the url
+        r = requests.get(iprange_url)  # make the request to the url
     except requests.RequestException as e:  # something went wrong with request
         print(e)
         raise e
@@ -32,8 +38,8 @@ def lambda_handler(event, context):
 
     try:
         response = ec2.describe_security_groups(
-            Filters=[dict(Name="group-name", Values=[GROUP_NAME])],
-            DryRun=DRY_RUN
+            Filters=[dict(Name="group-name", Values=[group_name])],
+            DryRun=dry_run
         )
     except Exception as e:
         print(f"Error getting security groups: {e}")
@@ -41,13 +47,13 @@ def lambda_handler(event, context):
 
     group = {}
     if len(response["SecurityGroups"]) == 0:  # make sure the group exists
-        if VERBOSE:
-            print("Security group "+GROUP_NAME+" does note exist, attempting to create it...")
+        if verbose:
+            print("Security group "+group_name+" does note exist, attempting to create it...")
         try:  # make the group if it doesn't exist
             response = ec2.create_security_group(
                 Description="CM Intern test security group",
-                GroupName=GROUP_NAME,
-                DryRun=DRY_RUN
+                GroupName=group_name,
+                DryRun=dry_run
             )
         except Exception as e:
             print(f"Security group did not exist, error creating security group: {e}")
@@ -60,13 +66,13 @@ def lambda_handler(event, context):
     # revoke all existing rules in the security group, so that we can add the new ones to update the group
     # don't bother deleting old permissions if there aren't any
     if "IpPermissions" in group and len(group["IpPermissions"]) > 0:
-        if VERBOSE:
+        if verbose:
             print("Old security group permissions:\n", group["IpPermissions"])
         try:
             ec2.revoke_security_group_ingress(
                 GroupId=group["GroupId"],
                 IpPermissions=group["IpPermissions"],
-                DryRun=DRY_RUN
+                DryRun=dry_run
             )
         except Exception as e:
             print(f"Error revoking security group rule: {e}")
@@ -91,12 +97,12 @@ def lambda_handler(event, context):
 
         ippermission[rangek] = [{
             cidrk: v,
-            "Description": GROUP_NAME + " " + v
+            "Description": group_name + " " + v
         }]
 
         permissions.append(ippermission)
 
-    if VERBOSE:
+    if verbose:
         print("Authorizing new security group permissions:\n", permissions)
 
     # authorize the new permissions
@@ -104,7 +110,7 @@ def lambda_handler(event, context):
         ec2.authorize_security_group_ingress(
             GroupId=group["GroupId"],
             IpPermissions=permissions,
-            DryRun=DRY_RUN
+            DryRun=dry_run
         )
     except Exception as e:
         print(f"Error authorizing security group rules: {e}")
